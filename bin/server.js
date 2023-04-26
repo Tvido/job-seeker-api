5f3c1eee1905bc79603fb68d5539ed9a5efef3c1
Volodymyr Viter <mad.tvido@gmail.com>
2023-04-26 17:59:44 +0300

    upd

--- bin/server.js
+++ bin/server.js
@@ -1,21 +1,9 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
-const app = require('../app');
+const mongoose = require("mongoose");
+require("dotenv").config();
+const app = require("../app");
 
-const { DB_HOST, PORT = 4000 } = process.env;
+const { PORT = 5000 } = process.env;
 
-mongoose
-  .connect(DB_HOST, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    app.listen(PORT, () => {
-      console.log(`Server running. Use our API on port: ${PORT}`);
-      console.log('Database connection successful');
-    });
-  })
-  .catch(error => {
-    console.log(error);
-    process.exit(1);
-  });
+app.listen(PORT, () => {
+  console.log(`Server running. Use our API on port: ${PORT}`);
+});
